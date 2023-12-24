9a82e3e821966c4ff6a55cb0a9b59424f061eca8
Sachin-0099 <itssachin0099@gmail.com>
2023-12-25 01:26:16 +0530

    Initial commit

---
+++ stringsfunction.java
@@ -0,0 +1,14 @@
+package DSA;
+
+public class stringsfunction {
+    public static void main(String args []){
+String str1="Coding";
+String str2=" Coding";
+// str1=str1+str2;
+// System.out.println(str1);
+System.out.println(str1.concat(str2));
+System.out.println(str1.equals(str2));
+System.out.println(str1.compareTo(str2));
+System.out.println(str1.contains("ing"));
+    }
+}
