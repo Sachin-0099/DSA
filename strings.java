a2d819799558ac54d64f41d68f9143e0de7c5129
Sachin-0099 <itssachin0099@gmail.com>
2023-12-25 01:07:15 +0530

    Initial commit

---
+++ strings.java
@@ -0,0 +1,13 @@
+package DSA;
+
+public class strings {
+    public static void main(String args []){
+        /*Strings are used for storing text.
+    A String variable contains a collection of characters surrounded by double quotes*/
+    String str="Sachin";
+    System.out.println(str);//to print string
+    System.out.println(str.length());//To check the length of the string
+    System.out.println(str.charAt(3));//To findout particular character from the string
+    
+    }
+}
