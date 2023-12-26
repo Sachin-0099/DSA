893c87e4f2a3e59e107dc92d05cd896a4f434166
Sachin-0099 <itssachin0099@gmail.com>
2023-12-27 00:36:51 +0530

    Iniital commit

---
+++ charString.java
@@ -0,0 +1,18 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class charString {
+    public static void  readchar(String str){
+        for(int i=0;i<str.length();i++){
+            System.out.println(str.charAt(i));
+        }
+    }
+public static void main(String args []){
+    Scanner sc =new Scanner(System.in);
+    System.out.println("Enter the strings");
+    String str=sc.nextLine();
+    readchar(str);
+
+}
+}
