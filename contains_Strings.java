4972a7300ae0aee47e7e34986b1f925f9f2b38ad
Sachin-0099 <itssachin0099@gmail.com>
2023-12-29 00:44:59 +0530

    Initial commit

---
+++ contains_Strings.java
@@ -0,0 +1,10 @@
+package DSA;
+
+public class contains_Strings {
+    public static void main(String args []){
+        String str="abc";
+        String str1="a";
+        System.out.println(str.contains(str1));
+
+    }
+}
