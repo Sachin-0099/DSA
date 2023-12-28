49c175ee9a3dfa0e13590e8b4dd9aca546862c0a
Sachin-0099 <itssachin0099@gmail.com>
2023-12-28 23:58:59 +0530

    Initial commit

---
+++ String_stored.java
@@ -0,0 +1,11 @@
+package DSA;
+
+public class String_stored {
+    public static void main(String args []){
+        String str1="Abc";
+        //String is no-primitive datatype we can also call string 2 this way as well
+        String str2=new String("Def");
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
