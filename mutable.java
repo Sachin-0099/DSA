eae7f1a74fa71e2cb04031ae628c1f0bcc42dc73
Sachin-0099 <itssachin0099@gmail.com>
2023-12-30 22:46:55 +0530

    Initial commit

---
+++ mutable.java
@@ -0,0 +1,12 @@
+package DSA;
+
+public class mutable {
+    public static void main(String args []){
+        //If we want mutable output then we will use StringBuffer
+        StringBuffer str=new StringBuffer("abc");
+        str.setCharAt(0, 'd');
+        System.out.println(str);
+        //Here we will get a mutable output
+
+    }
+}
